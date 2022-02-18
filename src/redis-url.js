3dfc39e3e194674c790e07ce8313baa0c29cd9a0
lichun <lchrennew@126.com>
2022-02-18 10:07:29 +0800

    first commit

---
+++ src/redis-url.js
@@ -0,0 +1,163 @@
+// redisUrl.js is used to create IORedis connections using an URL-like connection string
+// COPYRIGHT Lichun (lchrennew@126.com) 2022
+// version: 1.0.0
+import Redis from 'ioredis'
+
+const serverPattern = /^(?<host>[^:]+)(:(?<port>\d+))?$/
+const urlPattern = /^(?<protocol>[^:]+:)\/\/(?<pathname>[^\/?]+)[^?]*(\?(?<search>.*))?/
+const defaultPort = 6379
+
+
+export default class RedisURL {
+    /**
+     *
+     * @type {Array}
+     */
+    #servers
+    /**
+     *
+     * @type {URLSearchParams}
+     */
+    #entries
+    /**
+     *
+     * @type {string}
+     */
+    #protocol
+    /**
+     *
+     * @type {string}
+     */
+    #username
+    /**
+     *
+     * @type {string}
+     */
+    #password
+    /**
+     *
+     * @type {string}
+     */
+    #sentinelPassword
+
+    constructor(url = process.env.REDIS_URL, option = {
+        username: process.env.REDIS_USERNAME,
+        password: process.env.REDIS_PASSWORD,
+        sentinelPassword: process.env.REDIS_SENTINEL_PASSWORD,
+    }) {
+        this.#rawUrl = url
+        this.#username = option.username ?? process.env.REDIS_USERNAME
+        this.#password = option.password ?? process.env.REDIS_PASSWORD
+        this.#sentinelPassword = option.sentinelPassword ?? process.env.REDIS_SENTINEL_PASSWORD
+        const { protocol, search, pathname } = urlPattern.exec(url).groups
+        this.#entries = new URLSearchParams(search)
+        const servers = pathname.split(',').map(server => {
+            const { host, port = defaultPort } = serverPattern.exec(server).groups
+            return {
+                host,
+                port: Number(port),
+            }
+        })
+        this.#protocol = protocol
+        this.#servers = servers
+    }
+
+    /**
+     *
+     * @type {string}
+     */
+    #rawUrl
+
+    get rawUrl() {
+        return this.#rawUrl
+    }
+
+    get single() {
+        return [ 'redis:', 'rediss:' ].includes(this.#protocol)
+    }
+
+    get secured() {
+        return [ 'rediss:', 'rediss-sentinel:', 'rediss-cluster:' ].includes(this.#protocol)
+    }
+
+    get usesSentinel() {
+        return [ 'redis-sentinel:', 'rediss-sentinel:' ].includes(this.#protocol)
+    }
+
+    get clustered() {
+        return [ 'redis-cluster:', 'rediss-cluster:' ].includes(this.#protocol)
+    }
+
+    #getRedisOptions() {
+        const option = {}
+        option.name = this.#entries.get('name')
+        option.family = Number(this.#entries.get('family')) || 4
+        option.path = this.#entries.get('path') || null
+        option.keepAlive = Number(this.#entries.get('keepAlive')) || 0
+        option.noDelay = this.#entries.get('noDelay') !== 'false'
+        option.connectionName = this.#entries.get('connectionName') || null
+        option.db = Number(this.#entries.get('db')) || 0
+        option.dropBufferSupport = this.#entries.get('dropBufferSupport') === 'true'
+        option.enableReadyCheck = this.#entries.get('enableReadyCheck') !== 'false'
+        option.enableOfflineQueue = this.#entries.get('enableOfflineQueue') !== 'false'
+        option.connectTimeout = Number(this.#entries.get('connectTimeout')) || 10000
+        option.autoResubscribe = this.#entries.get('autoResubscribe') !== 'false'
+        option.autoResendUnfulfilledCommands = this.#entries.get('autoResendUnfulfilledCommands') !== 'false'
+        option.lazyConnect = this.#entries.get('lazyConnect') === 'true'
+        option.keyPrefix = this.#entries.get('keyPrefix') || ''
+        // option.tls = null // TLS connection support. See https://github.com/luin/ioredis#tls-options
+        // option.retryStrategy = null // https://github.com/luin/ioredis#auto-reconnect
+        option.maxRetriesPerRequest = Number(this.#entries.get('maxRetriesPerRequest')) || 0
+        option.reconnectOnError = null // https://github.com/luin/ioredis#auto-reconnect
+        option.readOnly = this.#entries.get('readOnly') === 'true'
+        option.stringNumbers = this.#entries.get('stringNumbers') === 'true'
+        option.enableAutoPipelining = this.#entries.get('enableAutoPipelining') === 'true'
+        option.autoPipeliningIgnoredCommands = (this.#entries.get('autoPipeliningIgnoredCommands') ?? '').split(',').filter(x => x)
+        option.maxScriptsCachingTime = Number(this.#entries.get('maxScriptsCachingTime')) || 60000
+        return option
+    }
+
+    #getClusterOptions() {
+        const option = {}
+        // option.clusterRetryStrategy
+        // option.dnsLookup
+        option.enableOfflineQueue = this.#entries.get('enableOfflineQueue') !== 'false'
+        option.enableReadyCheck = this.#entries.get('enableReadyCheck') !== 'false'
+        option.scaleReads = this.#entries.get('scaleReads') || 'master'
+        option.maxRedirections = Number(this.#entries.get('maxRedirections')) || 16
+        option.retryDelayOnFailover = Number(this.#entries.get('retryDelayOnFailover')) || 100
+        option.retryDelayOnClusterDown = Number(this.#entries.get('retryDelayOnClusterDown')) || 100
+        option.retryDelayOnTryAgain = Number(this.#entries.get('retryDelayOnTryAgain')) || 100
+        option.slotsRefreshTimeout = Number(this.#entries.get('slotsRefreshTimeout')) || 1000
+        option.slotsRefreshInterval = Number(this.#entries.get('slotsRefreshInterval')) || 5000
+        option.redisOptions = this.#getRedisOptions()
+        return option
+    }
+
+    /**
+     *
+     * @return {Redis|Cluster}
+     */
+    getRedis() {
+        if (this.usesSentinel) {
+            return new Redis({
+                ...this.#getRedisOptions(),
+                sentinels: this.#servers,
+                password: this.#password,
+                sentinelPassword: this.#sentinelPassword,
+            })
+        }
+        if (this.single) {
+            return new Redis({
+                ...this.#getRedisOptions(),
+                ...this.#servers[0],
+                password: this.#password,
+                username: this.#username,
+            })
+        }
+        if (this.clustered) {
+            return new Redis.Cluster(this.#servers, this.#getClusterOptions())
+        }
+        throw '不支持该协议'
+    }
+}
---
+++ src/redis-url.test.js
@@ -0,0 +1,102 @@
+import RedisURL from './redis-url.js';
+import jest from 'jest-mock'
+
+const use = async (url, option) => {
+    const redis = new RedisURL(url, option).getRedis()
+    return {
+        then: async f => {
+            await f(redis)
+            await redis.disconnect()
+        },
+    }
+}
+
+describe('综合', () => {
+    test('默认使用环境变量REDIS_URL', () => {
+        expect(new RedisURL().rawUrl).toEqual(process.env.REDIS_URL)
+    })
+
+    test('不支持的协议', async () => {
+        await expect(jest.fn(async () => await use('http://localhost'))).rejects.toBe('不支持该协议')
+    })
+})
+
+describe('redis单机', () => {
+    test('协议采用redis或rediss', () => {
+        expect(new RedisURL('redis://localhost').single).toBeTruthy()
+        expect(new RedisURL('rediss://localhost').single).toBeTruthy()
+
+        expect(new RedisURL('redis-sentinel://localhost').single).toBeFalsy()
+        expect(new RedisURL('rediss-sentinel://localhost').single).toBeFalsy()
+
+        expect(new RedisURL('redis-cluster://localhost').single).toBeFalsy()
+        expect(new RedisURL('rediss-cluster://localhost').single).toBeFalsy()
+    })
+
+    test('默认端口6379', async () => {
+        await use('redis://localhost').then(async redis => await expect(redis.options.port).toEqual(6379))
+    })
+
+    test('用户名默认使用环境变量REDIS_USERNAME', async () => {
+        await use('redis://localhost').then(async redis => await expect(redis.options.username).toBe(process.env.REDIS_USERNAME || null))
+        await use('redis://localhost', { username: '1234' }).then(async redis => await expect(redis.options.username).toEqual('1234'))
+    })
+
+    test('密码默认使用环境变量REDIS_PASSWORD', async () => {
+        await use('redis://localhost').then(redis => expect(redis.options.password).toBe(process.env.REDIS_PASSWORD || null))
+        await use('redis://localhost', { password: '1234' }).then(redis => expect(redis.options.password).toEqual('1234'))
+    })
+})
+
+describe('redis哨兵', () => {
+    test('协议采用redis-sentinel或rediss-sentinel', () => {
+        expect(new RedisURL('redis-sentinel://localhost').usesSentinel).toBeTruthy()
+        expect(new RedisURL('rediss-sentinel://localhost').usesSentinel).toBeTruthy()
+        expect(new RedisURL('redis://localhost').usesSentinel).toBeFalsy()
+        expect(new RedisURL('rediss://localhost').usesSentinel).toBeFalsy()
+        expect(new RedisURL('redis-cluster://localhost').usesSentinel).toBeFalsy()
+        expect(new RedisURL('rediss-cluster://localhost').usesSentinel).toBeFalsy()
+    })
+
+    test('必须提供master的名字', async () => {
+        await expect(jest.fn(async () => await use('redis-sentinel://localhost'))).rejects.toThrow('Requires the name of master.')
+    })
+
+    test('密码使用环境变量REDIS_SENTINEL_PASSWORD', async () => {
+        await use('redis-sentinel://localhost?name=mymaster')
+            .then(redis => expect(redis.options.sentinelPassword).toEqual(process.env.REDIS_SENTINEL_PASSWORD))
+        await use('redis-sentinel://localhost?name=mymaster', { password: '1234' })
+            .then(redis => expect(redis.options.sentinelPassword).toEqual('1234'))
+    })
+})
+
+describe('redis集群', () => {
+    test('协议采用redis-cluster或rediss-cluster', () => {
+        expect(new RedisURL('redis://localhost').clustered).toBeFalsy()
+        expect(new RedisURL('rediss://localhost').clustered).toBeFalsy()
+
+        expect(new RedisURL('redis-sentinel://localhost').clustered).toBeFalsy()
+        expect(new RedisURL('rediss-sentinel://localhost').clustered).toBeFalsy()
+
+        expect(new RedisURL('redis-cluster://localhost').clustered).toBeTruthy()
+        expect(new RedisURL('rediss-cluster://localhost').clustered).toBeTruthy()
+    })
+
+    test('集群', async () => {
+        await use('redis-cluster://localhost').then(redis => expect(redis.options.scaleReads).toEqual('master'))
+    })
+})
+
+describe('安全协议', () => {
+    test('rediss/rediss-sentinel/rediss-cluster都是安全协议', () => {
+        expect(new RedisURL('rediss://localhost').secured).toBeTruthy()
+        expect(new RedisURL('rediss-sentinel://localhost').secured).toBeTruthy()
+        expect(new RedisURL('rediss-cluster://localhost').secured).toBeTruthy()
+    })
+
+    test('redis/redis-sentinel/redis-cluster不是安全协议', () => {
+        expect(new RedisURL('redis://localhost').secured).toBeFalsy()
+        expect(new RedisURL('redis-sentinel://localhost').secured).toBeFalsy()
+        expect(new RedisURL('redis-cluster://localhost').secured).toBeFalsy()
+    })
+})
